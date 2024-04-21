c33b88d7e3f978d0d4f0d24fef57d357bf7542ac
guraril <guraril0716@gmail.com>
2024-04-21 14:19:39 +0900

    Add: install discord

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ main() {
             sudo nano /etc/pacman.conf
         fi
 
-        yay -S audacity blender dolphin fcitx5-im fcitx5-mozc firefox gimp google-chrome konsole lmms noto-fonts-cjk noto-fonts-emoji obs-studio plasma steam-native-runtime thunderbird tuxclocker ufw unityhub
+        yay -S audacity blender discord dolphin fcitx5-im fcitx5-mozc firefox gimp google-chrome konsole lmms noto-fonts-cjk noto-fonts-emoji obs-studio plasma steam-native-runtime thunderbird tuxclocker ufw unityhub
         sudo bash -c "echo XMODIFIERS=@im=fcitx5 > /etc/environment"
 
         sudo ufw default deny
