22c1fe695c80bed4fd69dd44f3ece4ad654e9127
guraril <guraril0716@gmail.com>
2024-04-24 21:33:22 +0900

    Fixes #1

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ main() {
         cd yay
         makepkg -si
 
+        # TODO: 冗長なので関数化する
         echo "Please enable multilib repo to continue installation."
         read -p "Press any key to continue." -n 1
         if type nvim >/dev/null 2>&1; then
@@ -30,6 +31,17 @@ main() {
             3) sudo pacman -Syyu nano;;
             *) sudo pacman -Syyu neovim;;
             esac
+
+            # TODO: 冗長なので関数化する
+            echo "Please enable multilib repo to continue installation."
+            read -p "Press any key to continue." -n 1
+            if type nvim >/dev/null 2>&1; then
+                sudo nvim /etc/pacman.conf
+            elif type vim >/dev/null 2>&1; then
+                sudo vim /etc/pacman.conf
+            elif type nano >/dev/null 2>&1; then
+                sudo nano /etc/pacman.conf
+            fi
         fi
 
         yay -S audacity blender discord dolphin fcitx5-im fcitx5-mozc firefox gimp google-chrome konsole lmms noto-fonts-cjk noto-fonts-emoji obs-studio plasma steam-native-runtime thunderbird tuxclocker unityhub
